dea354371f7395055735b4bce48cea127e02e920
Naveensai628 <73748808+Naveensai628@users.noreply.github.com>
2023-02-09 10:36:19 +0000

    first commit

---
+++ PLSQL Practice Code/create table query.sql
@@ -0,0 +1,27 @@
+create table test_file_export (
+id number primary key,
+user_id number,
+email_address varchar2(30),
+mobile_number varchar2(13),
+creation_date date,
+last_update_date date,
+due_date date,
+transaction_date date,
+primary_flag varchar2(1),
+constraint fkey_name 
+foreign key (user_id) references fnd_user (user_id)
+);
+
+
+create table fnd_user (
+user_id number primary key,
+user_name varchar2(30),
+email varchar2(20),
+mobile_number varchar2(15),
+job_position varchar2(40),
+creation_date date,
+last_update_date date
+);
+
+
+grant all on test_file_export to public;
